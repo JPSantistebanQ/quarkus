5efe088e7d626f8675fae1e8ae669c9e14fa0fdd
Juan Piero Santisteban Quiroz <jpsantq@gmail.com>
2025-01-02 09:48:11 -0500

    add: inital commit.

---
+++ smartbar-backoffice/src/test/java/com/jp/programming/ExampleResourceIT.java
@@ -0,0 +1,8 @@
+package com.jp.programming;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class ExampleResourceIT extends ExampleResourceTest {
+    // Execute the same tests but in packaged mode.
+}
